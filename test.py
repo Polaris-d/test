e8daa5c5aa216d777a0a3a7a38e5e66d58a46a8f
刘大伟 <610079251@qq.com>
2022-12-28 18:17:55 +0800

    ✨ feat(*): 测试

---
+++ test.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+'''
+@File    :   test.py
+@Time    :   2022/12/28 18:17:33
+@Author  :   Liu DaWei
+@Version :   1.0
+@Contact :   610079251@qq.com
+@Desc    :   None
+'''
+
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
